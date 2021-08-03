798a0b59f70398d75182c9555f07a9aec9210954
parkito <artyom-karnov@yandex.ru>
2021-08-02 23:45:06 -0400

    Add reactive base

---
+++ jvm/reactive/src/main/kotlin/ru/siksmfp/client/ClientApplication.kt
@@ -0,0 +1,4 @@
+package ru.siksmfp.client
+
+class ClientApplication {
+}
---
+++ jvm/reactive/src/test/kotlin/ru/siksmfp/server/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.siksmfp.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
